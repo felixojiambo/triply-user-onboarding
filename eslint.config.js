dcd1b89fe2d4bca8f4a9c5c62035b44628ab01b1
felixojiambo <felixojiamboe@gmail.com>
2025-05-09 00:53:31 +0300

    chore: add linting configuration with ESLint for Vue 3 project and setup lint script in package.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,55 +1,51 @@
 // eslint.config.js
-import vue from "eslint-plugin-vue";
-import vueParser from "vue-eslint-parser";
-import typescript from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import prettierPlugin from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import vue from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
+import typescript from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
   // Vue + TypeScript support in .vue files
   {
-    files: ["**/*.vue"],
+    files: ['**/*.vue'],
     languageOptions: {
       parser: vueParser,
       parserOptions: {
         parser: tsParser,
         ecmaVersion: 2020,
-        sourceType: "module",
+        sourceType: 'module',
       },
     },
     plugins: {
       vue,
-      "@typescript-eslint": typescript,
+      '@typescript-eslint': typescript,
       prettier: prettierPlugin,
     },
     rules: {
-      ...vue.configs["flat/recommended"].rules,
-      "@typescript-eslint/no-unused-vars": "warn",
-      "prettier/prettier": "error",
+      ...vue.configs['flat/recommended'].rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'prettier/prettier': 'error',
     },
   },
 
   // TypeScript-only files (non-Vue)
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: "module",
+        ecmaVersion: 2024,
+        sourceType: 'module',
       },
     },
     plugins: {
-      "@typescript-eslint": typescript,
+      '@typescript-eslint': typescript,
       prettier: prettierPlugin,
     },
     rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-      "prettier/prettier": "error",
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'prettier/prettier': 'error',
     },
   },
-
-  // Prettier config disables conflicting rules
-  prettierConfig,
 ];
