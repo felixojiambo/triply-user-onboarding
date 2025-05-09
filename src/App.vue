95064ed8634b337393817051bb1c404616f179be
felixojiambo <felixojiamboe@gmail.com>
2025-05-09 13:57:54 +0300

    chore(onboarding): wire App.vue to OnboardingWizard and configure Pinia plugin

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <script setup lang="ts">
-
+import OnboardingWizard from '@/components/OnboardingWizard.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+    <OnboardingWizard />
   </div>
-
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
