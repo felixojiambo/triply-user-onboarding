854408e2ab0186f5d8c535084d588da29a693b0a
felixojiambo <felixojiamboe@gmail.com>
2025-05-09 13:56:53 +0300

    feat(onboarding): implement Pinia store, autosave persistence & CI/CD gates

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { localStoragePlugin } from '@/plugins/piniaLocalStorage'
+import App from './App.vue'
 
-createApp(App).mount("#app");
+const pinia = createPinia()
+pinia.use(localStoragePlugin)
+
+createApp(App)
+  .use(pinia)
+  .mount('#app')
